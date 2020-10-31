129d37106eb3f6223b9f1a97d5d2d3f7696c2553
Simon Delamarre <simon.delamarre14@gmail.com>
2020-11-01 00:12:58 +0100

    Training seems to work !

--- transfoschtroumpf.py
+++ transfoschtroumpf.py
@@ -12,12 +12,16 @@ class SmurfTokenizer(PreTrainedTokenizer):
 
     def __init__(self, model_name="camembert-base", smurf_base_token="schtroumpf", space_symbol=SENTENCE_PIECE_SPACE,
                  **kwargs):
+        super().__init__()
         self.base_tokenizer = AutoTokenizer.from_pretrained(model_name)
         self.smurf_base_token = smurf_base_token
         self.space_symbol = space_symbol
         self.all_smurf_tokens = [smurf_base_token, self.space_symbol + self.smurf_base_token,
                                  self.space_symbol + self.smurf_base_token.capitalize()]
-        super().__init__()
+        self.pad_token = self.base_tokenizer.pad_token
+        self.mask_token = self.base_tokenizer.mask_token
+        self.eos_token = self.base_tokenizer.eos_token
+        self.model_max_length = self.base_tokenizer.model_max_length
 
     def build_inputs_with_special_tokens(self, token_ids_0: List, token_ids_1: Optional[List] = None) -> List:
         return self.base_tokenizer.build_inputs_with_special_tokens(token_ids_0, token_ids_1)
@@ -83,9 +87,10 @@ class SmurfTokenizer(PreTrainedTokenizer):
                     next_tokens.pop(0)
             return previous_tokens + [smurf_token] + next_tokens
 
-from transformers import AutoModel, RobertaConfig, RobertaModel, RobertaForCausalLM, RobertaForCausalLM, modeling_outputs
 
-from torch import Tensor, nn
+from transformers import AutoModel, RobertaConfig, RobertaForCausalLM, modeling_outputs, Trainer, TrainingArguments
+
+from torch import Tensor, nn, utils
 class TransfoSchtroumpf(nn.Module):
       def __init__(self):
         super().__init__()
@@ -134,5 +139,81 @@ transfoschtroumpf = TransfoSchtroumpf()
 #print(test)
 
 smurf_tok = SmurfTokenizer()
-data = smurf_tok("Je me schtroumpferai jusqu'à la mort !", return_tensors="pt")
-print(transfoschtroumpf(**data, labels=smurf_tok("Je me battrai jusqu'à la mort !", return_tensors="pt")["input_ids"]))
+#data = smurf_tok("Je me schtroumpferai jusqu'à la mort !", return_tensors="pt")
+#print(transfoschtroumpf(**data, labels=smurf_tok("Je me battrai jusqu'à la mort !", return_tensors="pt")["input_ids"]))
+
+import torch
+class SmurfDataset(torch.utils.data.Dataset):
+    def __init__(self, encodings, labels):
+        self.encodings = encodings
+        self.labels = labels
+
+    def __getitem__(self, idx):
+        item = {key: val[idx] for key, val in self.encodings.items()}
+        item['labels'] = self.labels['input_ids'][idx]
+        return item
+
+    def __len__(self):
+        return len(self.labels['input_ids'])
+
+all_raw_data = [("Je me schtroumpferai jusqu'à la mort !",
+                   "Je me battrai jusqu'à la mort !"),
+                  ("Qu'est-ce qu'il fait schtroumpf ! J'aurais dû prendre une écharpe !",
+                   "Qu'est-ce qu'il fait froid ! J'aurais dû prendre une écharpe !"),
+                  ("Il va falloir schtroumpfer par le gué !",
+                   "Il va falloir passer par le gué !"),
+                  ("Schtroumpfons une petite chanson de marche pour nous schtroumpfer du courage !",
+                   "Chantons une petite chanson de marche pour nous donner du courage !"),
+                  ("Trois petites lieues à pied, ça schtroumpfe, ça schtroumpfe !",
+                   "Trois petites lieues à pied, ça use, ça use !"),
+                  ("Il va nous schtroumpfer comme des souris !",
+                   "Il va nous croquer comme des souris !"),
+                  ("Pfff ! On l'a schtroumpfé belle !",
+                   "Pfff ! On l'a échappé belle !")]
+
+dataset_size = len(all_raw_data)
+train_size = int(dataset_size*0.8)
+
+#train_raw_data = all_raw_data[:train_size]
+#eval_raw_data = all_raw_data[train_size:]
+train_raw_data = all_raw_data
+eval_raw_data = all_raw_data
+
+train_encoded_inputs = smurf_tok([x for (x, y) in train_raw_data], padding=True, truncation=True)
+train_encoded_labels = smurf_tok([y for (x, y) in train_raw_data], padding=True, truncation=True)
+
+eval_encoded_inputs = smurf_tok([x for (x, y) in eval_raw_data], padding=True, truncation=True)
+eval_encoded_labels = smurf_tok([y for (x, y) in eval_raw_data], padding=True, truncation=True)
+
+train_dataset = SmurfDataset(train_encoded_inputs, train_encoded_labels)
+eval_dataset = SmurfDataset(eval_encoded_inputs, eval_encoded_labels)
+
+model = TransfoSchtroumpf()
+
+
+from transformers import Trainer, TrainingArguments
+training_args = TrainingArguments(
+    output_dir='./results',          # output directory
+    num_train_epochs=1024,              # total # of training epochs
+    per_device_train_batch_size=16,  # batch size per device during training
+    per_device_eval_batch_size=64,   # batch size for evaluation
+    warmup_steps=500,                # number of warmup steps for learning rate scheduler
+    weight_decay=0.01,               # strength of weight decay
+    logging_steps=10,
+    logging_dir='./logs',            # directory for storing logs
+)
+
+trainer = Trainer(
+    tokenizer=smurf_tok,
+    model=model,                         # the instantiated 🤗 Transformers model to be trained
+    args=training_args,                  # training arguments, defined above
+    train_dataset=train_dataset,         # training dataset
+    eval_dataset=eval_dataset            # evaluation dataset
+)
+
+print("Training...")
+trainer.train()
+print("Evaluating...")
+print(trainer.evaluate())
+#print("Saving model...")
+#model.save_pretrained("saved_models")
